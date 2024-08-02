10208ba132be869864437574f790dc77be7cbb9b
AVIRENI CHANDAN <avireni.chandanchandu43657@gmail.com>
2024-08-03 00:44:52 +0530

    Adding the data scraped to a workbook as output.

--- application.py
+++ application.py
@@ -1,5 +1,7 @@
 from collections import defaultdict
 
+from openpyxl import Workbook
+
 from web_scraping import get_all_tables
 
 
@@ -10,11 +12,19 @@ def calculate_winner_looser(data):
         teams_by_year[team["Year"]].append(team)
 
     # Find teams with max and min wins for each year
-    results = {}
+    results = []
     for year, teams in teams_by_year.items():
         max_team = max(teams, key=lambda x: x["Wins"])
         min_team = min(teams, key=lambda x: x["Wins"])
-        results[year] = {"max": max_team, "min": min_team}
+        results.append(
+            {
+                "Year": year,
+                "Winner": max_team["Team Name"],
+                "Winner Num. of Wins": max_team["Wins"],
+                "Loser": min_team["Team Name"],
+                "Loser Num. of Wins": min_team["Wins"],
+            }
+        )
 
     return results
 
@@ -30,3 +40,26 @@ if __name__ == "__main__":
 
     results = calculate_winner_looser(records)
     print(results, file=open("output.txt", "w"))
+
+    # Create a workbook and select the active worksheet
+    wb = Workbook()
+    ws1 = wb.active
+    ws1.title = "NHL Stats 1990-2011"
+
+    # Write the first sheet data
+    headers1 = list(records[0].keys())
+    ws1.append(headers1)
+    for entry in records:
+        ws1.append(list(entry.values()))
+
+    # Create a second worksheet
+    ws2 = wb.create_sheet(title="Winner and Loser per Year")
+
+    # Write the second sheet data
+    headers2 = list(results[0].keys())
+    ws2.append(headers2)
+    for entry in results:
+        ws2.append(list(entry.values()))
+
+    # Save the workbook
+    wb.save("teams_and_players_data.xlsx")
