6f8f4eba73069d729547ee3991606b4309740072
AVIRENI CHANDAN <avireni.chandanchandu43657@gmail.com>
2024-08-02 21:41:22 +0530

    Application code updated.

--- application.py
+++ application.py
@@ -1,9 +1,10 @@
-# print(get_table_data("https://www.scrapethissite.com/pages/forms/?page_num=1"))
 from web_scraping import get_all_tables
 
 if __name__ == "__main__":
-    total_pages = 24  # Replace with the total number of pages you want to scrape
+    # Fetch the data from the website
+    total_pages = 24
     base_url = "https://www.scrapethissite.com/pages/forms/"
 
     records = get_all_tables(total_pages, base_url)
+
     print(records, len(records))
